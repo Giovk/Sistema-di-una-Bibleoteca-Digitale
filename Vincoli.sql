5fb4ce1be666dbc4aa27c3a413d62fa92aa958f7
Giovk <giovkriccio@libero.it>
2022-12-16 14:18:32 +0100

    Update Vincoli.sql

--- Vincoli.sql
+++ Vincoli.sql
@@ -8,3 +8,16 @@ CHECK(
 
     )
 );
+
+
+-- Un articolo scientifico non può essere pubblicato in una 
+-- rivista prima della pubblicazione della rivista e dell'articolo
+CREATE ASSERTION A2
+CHECK(
+    NOT EXISTS(
+        SELECT *
+        FROM ((COMPOSIZIONE AS C JOIN RIVISTA AS R ON C.ISSN = R.ISSN)
+            JOIN ARTICOLO_SCIENTIFICO AS AR ON AR.CodAS = C.CodAS)
+        WHERE C.DataInserimento < R.DataPubblicazione OR YEAR(C.DataInserimento) < AR.AnnoPubblicazione 
+    )
+);
