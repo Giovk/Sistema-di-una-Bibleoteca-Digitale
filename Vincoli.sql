00289dca3cfe54256b4b110b60a768cc502d2710
Giovk <giovkriccio@libero.it>
2022-12-16 12:47:49 +0100

    Update Vincoli.sql

--- Vincoli.sql
+++ Vincoli.sql
@@ -1,15 +1,3 @@
--- Non possono esserci, quando specificate, valori duplicati dell'attributo 'partitaIVA'
-ALTER TABLE UTENTE
-ADD CONSTRAINT C1
-CHECK(
-    NOT EXISTS(
-        SELECT COUNT(Username)
-        FROM UTENTE
-        GROUP BY PartitaIVA
-        HAVING COUNT(Username) > 1
-    )
-);
-
 -- Una libreria può essere gestita solo da utenti con una partitaIVa
 CREATE ASSERTION A1
 CHECK(
