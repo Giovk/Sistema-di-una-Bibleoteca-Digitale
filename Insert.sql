a2bef220d070391e3b879a3443a0dc4e41141f5e
Giovk <giovkriccio@libero.it>
2022-12-27 20:24:45 +0100

    Update Insert.sql

---
+++ Insert.sql
@@ -0,0 +1,65 @@
+INSERT INTO UTENTE (Username, Email, pw, PartitaIva, Nome, Cognome)
+VALUES ('Admin1', 'giovanni.riccio6@studenti.unina.it', 'admin', '00000000001', 'Giovanni', 'Riccio');
+
+INSERT INTO UTENTE (Username, Email, pw, PartitaIva, Nome, Cognome)
+VALUES ('Admin2', 'gen.nappo@studenti.unina.it', 'admin', '00000000002', 'Gennaro', 'Nappo');
+
+INSERT INTO UTENTE (Username, Email, pw, PartitaIva, Nome, Cognome)
+VALUES ('Giudil', 'giulio.dilorenzo@gmail.com', 'a1b2c3', '73291990775', 'Giulio', 'Di Lorenzo');
+
+INSERT INTO UTENTE (Username, Email, pw, PartitaIva, Nome, Cognome)
+VALUES ('Irmluc', 'irma.lucciano@gmail.com', 'd4e5f6', '48645480756', 'Irma', 'Lucciano');
+
+INSERT INTO UTENTE (Username, Email, pw, PartitaIva, Nome, Cognome)
+VALUES ('Rosbru', 'rosario.bruno@gmail.com', 'g7h8i9', '31265250287', 'Rosario', 'Bruno');
+
+INSERT INTO UTENTE (Username, Email, pw, PartitaIva, Nome, Cognome)
+VALUES ('Waffer', 'walter.fallacci@gmail.com', 'l0m1n2', '73073560895', 'Walter', 'Fallacci');
+
+INSERT INTO UTENTE (Username, Email, pw, PartitaIva, Nome, Cognome)
+VALUES ('Benriz', 'bene.rizzo@gmail.com', 'o2p4q5', '39364160828', 'Benedetta', 'Rizzo');
+
+INSERT INTO UTENTE (Username, Email, pw, PartitaIva, Nome, Cognome)
+VALUES ('DonRodrigo', 'manzoni.lover@libero.it', 'amolucia1327', '43511610404', 'Rodrigo', 'Padovano');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Flomil', 'floramilano@outlook.com', 'r6s7t8','Flora', 'Milano');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Corinnio', 'corinna.onnio88@gmail.com', 'pretzel15', 'Corinna', 'Onnio');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Etzio', 'ettore.mazzanti@libero.it', 'u9v0z1', 'Ettore', 'Mazzanti');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Elio69', 'eliogreco68@outlook.com', 'ilovechimica12', 'Elio', 'Greco');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Fernindx', 'Fernando.napoli@libero.it', 'Napoli2003', 'Fernando', 'Arcuri');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('CatCat', 'cat.lover.2002@gmail.com', 'animeloverNYA', 'Caterina', 'Cattaneo');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('ConteDiCavour', 'Camillo.purosangue@outlook.com', 'prince123', 'Camillo', 'Bellucci');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Baguette', 'rosetta.scarpetta@paninarodifiducia.it', 'ilovebakery33', 'Luciano', 'Pagnotto');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Imma', 'hounferrointasca@gmail.com', 'ak-42<3', 'Imma', 'Ferri');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Porticata', 'sara.cinesca@libero.it', 'sarachiusa?', 'Sara', 'Cinesca');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('ErPupone', 'ilary.mi.ha.lasciato@roma.it', 'Siefragatairolex', 'Francesco', 'Totti');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Leoncino', 'ilkingdellagiungla@gmail.com', 'Staseramisentounleone33', 'Leon', 'Cino');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Cruzz_o_portier', 'popopopo@gmail.com', 'fozzanapoli', 'Ciro', 'Esposito');
+
+INSERT INTO UTENTE (Username, Email, pw, Nome, Cognome)
+VALUES ('Bocciolo', 'pratofiorito@outlook.com', 'fiordariso11', 'Fiore', 'Dell Orto');
