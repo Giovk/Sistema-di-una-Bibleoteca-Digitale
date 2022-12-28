fb6f1f9fd01264d35413e90393e8271a756a1adb
Giovk <giovkriccio@libero.it>
2022-12-28 19:24:23 +0100

    Update Insert.sql

--- Insert.sql
+++ Insert.sql
@@ -98,8 +98,6 @@ VALUES ('0026-4423', 'GEDI Gruppo Editoriale', 'Psicologia', 'Cattaneo', 'Marco'
 INSERT INTO RIVISTA (ISSN, Editore, Argomento, CognomeR, NomeR, Titolo, AnnoPubblicazione)
 VALUES ('2724-2641', 'Gruppo B Editore', 'Scienze', 'Hack', 'Margherita', 'Le Stelle', 2002);
 INSERT INTO RIVISTA (ISSN, Editore, Argomento, CognomeR, NomeR, Titolo, AnnoPubblicazione)
-VALUES ('2284-4422', 'EDI HOUSE sas', 'Scienze', 'Biasion', 'Giulio', 'Voyager Magazine',2012);
-INSERT INTO RIVISTA (ISSN, Editore, Argomento, CognomeR, NomeR, Titolo, AnnoPubblicazione)
 VALUES ('2531-3975', 'Giovanna Truda', 'Scienze Umane', 'Fangone', 'Emiliana', 'Culture e Studi del Sociale', 2022);
 INSERT INTO RIVISTA (ISSN, Editore, Argomento, CognomeR, NomeR, Titolo, AnnoPubblicazione)
 VALUES ('1357-1915', 'Arnoldo Mondadori Editore', 'Scienze', 'Loredan', 'Jacopo', 'Focus', 1992);
@@ -111,34 +109,357 @@ VALUES ('0036-4681', 'Edizioni Dedalo', 'Scienze', 'Armaroli', 'Nicola', 'Sapere
 -- Articolo Scientifico --
 
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00000', '', );
+VALUES ('10-00001', "Che cosa significa allineare l''inteliggenza artificiale ai valori umani?", 2022); 
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00001', '', );
+VALUES ('10-00002', 'Evaluating Understanding on Conceptual Abstraction Benchmarks', 2022);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00002', '', );
+VALUES ('10-00003', 'Un''intelligenza artificiale è capace di prevedere le nuove sostanze psicoattive', 2022);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00003', '', );
+VALUES ('10-00004', 'A volte ritornano? Viaggio tra i tentativi di riportare in vita "il mondo perduto"', 2019);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00004', '', );
+VALUES ('10-00005', 'Uguale o diverso? Le reti neurali stentano a rispondere', 2021);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00005', '', );
+VALUES ('10-00006', 'Combattere i tumori con l''intelligenza artificiale', 2020);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00006', '', );
+VALUES ('10-00007', 'Computers tell us who to date, who to jail: But should they?', 2018);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00007', '', );
+VALUES ('10-00008', 'How Your Brain May One Day Control Your Computer', 2018);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00008', '', );
+VALUES ('10-00009', 'Computers Decipher Burnt Scroll Found in Ancient Holy Ark', 2016);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00009', '', );
+VALUES ('10-00010', 'For girls in science, the time is now', 2019);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00010', '', );
+VALUES ('10-00011', 'How will this pandemic disrupt science?', 2020);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00011', '', );
+VALUES ('10-00012', 'Bluer blues and blacker blacks? How science is enhancing colors', 2020);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00012', '', );
+VALUES ('10-00013', 'La mente e l''ambiente', 2022);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00013', '', );
+VALUES ('10-00014', 'Psichiatria digitale', 2022);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00014', '', );
+VALUES ('10-00015', 'A mente fredda', 2022);
 INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
-VALUES ('10-00015', '', );
+VALUES ('10-00016', 'Lenire il trauma con la scrittura', 2022);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00017', 'Una nuova psichiatria', 2022);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00018', 'Le strutture della corona', 2006);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00019', 'La distanza della Stella di Teegarden', 2006);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00020', 'SBIG DSS7 astrofisica', 2006);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00021', 'Megastelle', 2006);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00022', 'Le prime luci sull’Era Oscura', 2006);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00023', 'Che fine farà la nuova macchia di Giove?', 2006);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00024', 'Una camera planetaria eccellente', 2006);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00025', 'Un quasar in ogni galassia?', 2006);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00026', 'Nebulosa Granchio: chi la vide nascere', 2006);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00027', 'Per un welfare sostenibile e innovativo', 2016);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00028', 'Il futuro della cooperazione sociale nelle ipotesi di welfare', 2016);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00029', 'Pratica sportiva e lavoro sociale tra stato, mercato e comunità', 2017);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00030', 'Migrazioni mediterranee: Tra allarmismo e risorsa sociale', 2017);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00031', 'Per una cittadinanza globale: Percorsi di vita e traiettorie formative dei giovani', 2018);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00032', 'Il ruolo culturale della comunicazione sociale', 2018);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00033', 'Le lenti a contatto intelligenti', 2022);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00034', 'L’ologramma del primo tatuaggio NFT realizzato nel metaverso', 2022);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00035', 'Internet: quanto lo usiamo?', 2022);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00036', 'Quali sono le password più sicure?', 2022);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00037', 'Un esame virtuale per rinnovare la patente?', 2022);
+INSERT INTO ARTICOLO_SCIENTIFICO (DOI, Titolo, AnnoPubblicazione)
+VALUES ('10-00038', 'Una tempesta solare può provocare un''apocalisse digitale', 2022);
+
+-- AUTORE --
+
+INSERT INTO AUTORE (Nome, Cognome)
+VALUES ('Melanie', 'Mitchell');
+INSERT INTO AUTORE (Nome, Cognome)
+VALUES ('Victor Vikram', 'Odouard');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Massimo', 'Sandal', 'Italiana');
+INSERT INTO AUTORE (Nome, Cognome)
+VALUES ('John', 'Pavlus');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Esther', 'Landhuis', 'Statunitense');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Simon', 'Worrall', 'Inglese', '1968-06-18');
+INSERT INTO AUTORE (Nome, Cognome)
+VALUES ('Christina', 'Nunez');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Michael', 'Greshko', 'Statunitense');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Claudia', 'Kalb', 'Cinese');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Victoria', 'Jaggard', 'Statunitense', '1999-07-02');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Sarah', 'Gibbens', 'Statunitense');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Giulia', 'Mondanini', 'Italiana');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Paola Emilia', 'Cicerone', 'Italiana');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Frank', 'Luerweg', 'Tedesca');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Nayla', 'Chidiac', 'Francese');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Mattia', 'Maccarone', 'Italiana');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Maurice', 'Hamilton', 'Canadese', '1923-04-04');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Tom', 'Stafford', 'Statunitense', '1930-09-17');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Flavio', 'Castellani', 'Italiana', '1957-11-23');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Marcello', 'Felli', 'Italiana', '1920-06-25');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Geof', 'Macdonald', 'Canadese');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Volker', 'Bromm', 'Statunitense');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Edwin', 'Aguirre', 'Canadese');
+INSERT INTO AUTORE (Nome, Cognome)
+VALUES ('Sean', 'Walker');
+INSERT INTO AUTORE (Nome, Cognome)
+VALUES ('Robert', 'Irion');
+INSERT INTO AUTORE (Nome, Cognome, DataNascita)
+VALUES ('Francis Richard', 'Stephenson', '1941-04-26');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('David', 'Green', 'Inglese');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Emiliana', 'Mangone', 'Italiana', '1970-02-02');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Pierpaolo', 'Donati', 'Italiana', '1946-09-30');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Francesco', 'Pirone', 'Italiana');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Claudio', 'Marra', 'Italiana', '1959-06-24');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Paolo', 'Diana', 'Italiana', '1992-02-02');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Michela', 'Freddano', 'Italiana');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Francesco', 'Pira', 'Italiana', '1965-08-09');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Antonia', 'Cava', 'Italiana');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita)
+VALUES ('Rebecca', 'Mantovani', 'Italiana');
+INSERT INTO AUTORE (Nome, Cognome)
+VALUES ('Unknown', 'Unknown');
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('Chiara', 'Guzzonato', 'Italiana', '1974-02-23'); 
+INSERT INTO AUTORE (Nome, Cognome, Nazionalita, DataNascita)
+VALUES ('', '', '', '');
+
+-- SCRITTURA_A --
+
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00001', 1);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00002', 1);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00002', 2);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00003', 3);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00004', 3);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00005', 4);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00006', 5);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00007', 6);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00008', 7);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00009', 8);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00010', 9);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00011', 10);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00012', 11);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00013', 12);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00014', 13);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00015', 14);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00016', 15);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00017', 16);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00018', 17);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00019', 18);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00020', 19);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00021', 20);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00021', 21);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00022', 22);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00023', 23);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00024', 24);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00025', 25);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00026', 26);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00026', 27);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00027', 28);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00028', 29);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00029', 30);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00030', 31);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00031', 32);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00031', 33);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00032', 34);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00032', 35);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00033', 36);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00034', 37);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00035', 38);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00036', 38);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00037', 36);
+INSERT INTO SCRITTURA_A (DOI, CodA)
+VALUES ('10-00038', 36);
+
+-- FASCICOLO --
+
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (1, 'GEDI Gruppo Editoriale', '2021-12-28', '0036-8083');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (2, 'GEDI Gruppo Editoriale', '2022-01-28', '0036-8083');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (1, 'National Geographic Society', '2020-01-01', '0027-9358');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (2, 'National Geographic Society', '2020-02-01', '0027-9358');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (1, 'GEDI Gruppo Editoriale', '2022-12-05', '0026-4423');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (42, 'Gruppo B Editore', '2006-07-29', '2724-2641');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (43, 'Gruppo B Editore', '2006-08-27', '2724-2641');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (44, 'Gruppo B Editore', '2006-09-28', '2724-2641');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (1, 'Giovanna Truda', '2016-06-27', '2531-3975');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (2, 'Giovanna Truda', '2017-06-22', '2531-3975');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (3, 'Giovanna Truda', '2018-06-28', '2531-3975');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (1, 'Arnoldo Mondadori Editore', '2022-12-01', '1357-1915');
+INSERT INTO FASCICOLO (Numero, Editore, DataPubblicazione, ISSN)
+VALUES (2, 'Arnoldo Mondadori Editore', '2023-01-01', '1357-1915');
+
+-- INTRODUZIONE --
+
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (2, '10-00001');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (2, '10-00002');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (2, '10-00003');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (1, '10-00004');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (1, '10-00005');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (1, '10-00006');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (3, '10-00007');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (3, '10-00008');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (3, '10-00009');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (4, '10-00010');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (4, '10-00011');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (4, '10-00012');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (5, '10-00013');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (5, '10-00014');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (5, '10-00015');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (5, '10-00016');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (5, '10-00017');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (6, '10-00018');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (6, '10-00019');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (6, '10-00020');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (6, '10-00021');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (7, '10-00022');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (7, '10-00023');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (7, '10-00024');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (8, '10-00025');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (8, '10-00026');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (9, '10-00027');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (9, '10-00028');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (10, '10-00029');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (10, '10-00030');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (11, '10-00031');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (11, '10-00032');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (12, '10-00033');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (12, '10-00034');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (12, '10-00035');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (13, '10-00036');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (13, '10-00037');
+INSERT INTO INTRODUZIONE (CodF, DOI)
+VALUES (13, '10-00038');
+
