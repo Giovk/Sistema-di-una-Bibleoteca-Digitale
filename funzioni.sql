4540963fc39c4f69b1e364728aa5981d0084dc6c
Giovk <giovkriccio@libero.it>
2023-01-23 11:21:51 +0100

    Update funzioni.sql

--- funzioni.sql
+++ funzioni.sql
@@ -682,13 +682,13 @@ BEGIN
 
     testo_notifica='NOTIFICA: La serie '||titolo_serie||' è completamente disponibile in formato '||fruizione||' alla libreria '||nome_libreria;
 
-    --IF indirizzo_libreria IS NOT NULL AND sito IS NOT NULL THEN
-    --    testo_notifica=testo_notifica||' presso '||indirizzo_libreria||' oppure al sito: '||sito;
-    --ELSIF indirizzo_libreria IS NULL THEN
-    --    testo_notifica=testo_notifica||' al sito: '||sito;
-    --ELSE
-    --    testo_notifica=testo_notifica||' presso '||indirizzo_libreria;
-    --END IF;
+    IF indirizzo_libreria IS NOT NULL AND sito IS NOT NULL THEN
+        testo_notifica=testo_notifica||' presso '||indirizzo_libreria||' oppure al sito: '||sito;
+    ELSIF indirizzo_libreria IS NULL THEN
+        testo_notifica=testo_notifica||' al sito: '||sito;
+    ELSE
+        testo_notifica=testo_notifica||' presso '||indirizzo_libreria;
+    END IF;
 
     testo_notifica=testo_notifica||'.';
 
