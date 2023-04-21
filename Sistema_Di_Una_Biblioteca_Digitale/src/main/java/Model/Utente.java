0529efd9c94af4ec59ce9230cfe8d5633d9595ca
Giovk <giovkriccio@libero.it>
2023-04-21 17:24:49 +0200

    Update Utente.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Utente.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Utente.java
@@ -1,4 +1,23 @@
 package Model;
 
+import java.util.Date;
+
 public class Utente {
+    public String username;
+    public String password;
+    public String email;
+    public String nome;
+    public String cognome;
+    public Date dataNascita;
+    public String partitaIVA;
+
+    public Utente (String u, String p, String e, String n, String c, Date dn, String pIVA) {
+        username = u;
+        password = p;
+        email = e;
+        nome = n;
+        cognome = c;
+        dataNascita = dn;
+        partitaIVA = pIVA;
+    }
 }
