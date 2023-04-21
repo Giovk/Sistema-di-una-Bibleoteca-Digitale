034d07403b159c426d1523d1a1c4ee8951c8b23d
Giovk <giovkriccio@libero.it>
2023-04-21 17:24:33 +0200

    Create Elemento.java

---
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Elemento.java
@@ -0,0 +1,13 @@
+package Model;
+
+import java.util.Date;
+
+public class Elemento {
+    public String titolo;
+    public Date dataPubblicazione;
+
+    public Elemento(String t, Date dp){
+        titolo = t;
+        dataPubblicazione = dp;
+    }
+}
