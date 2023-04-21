8ac7628538edbdfed1e1aeb4c00b80885a9e85ff
Giovk <giovkriccio@libero.it>
2023-04-21 17:24:38 +0200

    Create Libreria.java

---
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Libreria.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Libreria {
+    public String nome;
+    public String numeroTelefonico;
+    public String indirizzo;
+    public String sitoWeb;
+    public Utente gestore;
+
+    public Libreria(String n, String nt, String i, String sw, Utente g){
+        nome = n;
+        numeroTelefonico = nt;
+        indirizzo = i;
+        sitoWeb = sw;
+        gestore = g;
+    }
+}
