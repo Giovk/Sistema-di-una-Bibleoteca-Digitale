05036049c84ffc18a0d4d00c0f85c2ade914fe90
Giovk <giovkriccio@libero.it>
2023-04-21 18:28:25 +0200

    Create Autore.java

---
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Autore.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Autore {
+    public String nome;
+    public String cognome;
+    public String nazionalita;
+    public Date dataNascita;
+    public ArrayList<ArticoloScientifico> articoliScritti;
+
+    public Autore(String no, String c, String na, Date dn){
+        nome = no;
+        cognome = c;
+        nazionalita = na;
+        dataNascita = dn;
+    }
+}
