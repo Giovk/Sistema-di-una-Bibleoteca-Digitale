2b2bfcac1169985ff4e4f3356b8a2603a91a9e15
Giovk <giovkriccio@libero.it>
2023-04-22 12:28:54 +0200

    Update Collana.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Collana.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Collana.java
@@ -1,4 +1,20 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Collana {
+    public String caratteristica;
+    public String nome;
+    public String isbn;
+    public ArrayList<Libro> libri;
+
+    public Collana(String c, String n, String i, ArrayList<Libro> l){
+        caratteristica = c;
+        nome = n;
+        isbn = i;
+
+        for (Libro libroAttuale : l){
+            libri.add(libroAttuale);
+        }
+    }
 }
