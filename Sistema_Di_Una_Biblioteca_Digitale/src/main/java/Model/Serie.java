4500ebeb789f77d63d43490f78d2c360607e545e
Giovk <giovkriccio@libero.it>
2023-04-22 12:41:20 +0200

    Update Serie.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Serie.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Serie.java
@@ -1,4 +1,21 @@
 package Model;
 
-public class Serie {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Serie extends Elemento{
+    public String isbn;
+    public int nLibri;
+    public ArrayList<Libro> libri;
+
+    public Serie(String i, int nl, ArrayList<Libro> l, String t, Date dp){
+        super(t, dp);
+
+        isbn = i;
+        nLibri = nl;
+
+        for (Libro libroAttuale : l){
+            libri.add(libroAttuale);
+        }
+    }
 }
