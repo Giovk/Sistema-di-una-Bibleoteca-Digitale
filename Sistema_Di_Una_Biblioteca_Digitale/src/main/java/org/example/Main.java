0c328286049b1ba1b4afd6d5f9de54471c296a1e
Giovk <giovkriccio@libero.it>
2023-05-02 11:49:25 +0200

    Update Main.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/org/example/Main.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import GUI.GuiTry;
+import GUI.LSForm;
 import ImplementazionePostgresDAO.LibroImplementazionePostgresDAO;
 
 public class Main {
     public static void main(String[] args){
         LibroImplementazionePostgresDAO l = new LibroImplementazionePostgresDAO();
         l.printLibri();
+        new GuiTry();
     }
 }
