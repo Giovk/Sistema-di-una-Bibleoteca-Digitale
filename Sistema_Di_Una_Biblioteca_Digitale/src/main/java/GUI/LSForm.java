53fcf47f2ae3a819ee2ec79f069026e3565e22ac
Giovk <giovkriccio@libero.it>
2023-05-02 11:49:22 +0200

    Create LSForm.java

---
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/LSForm.java
@@ -0,0 +1,44 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class LSForm{
+    private JFrame frame;
+    private JButton btLogin;
+    private JButton btSignIn;
+    private JLabel txtPF;
+    private JLabel image;
+    private JPanel lsPanel;
+
+    public LSForm() {
+        btLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LSDialog lsDialog = new LSDialog(0);
+                btLogin.setEnabled(false);
+                btSignIn.setEnabled(false);
+            }
+        });
+        btSignIn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LSDialog lsDialog = new LSDialog(1);
+                btLogin.setEnabled(false);
+                btSignIn.setEnabled(false);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Biblioteca Digitale");
+        frame.setUndecorated(true);
+        frame.setContentPane(new LSForm().lsPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(1280,720);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+}
