3e37ea815aff0ba9e50100fd1c52da0f9f675aae
giovk <giovkriccio@libero.it>
2023-05-25 19:16:52 +0200

    Update Program

---
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/test.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class test
+{
+    private JButton homeButton;
+    private JButton libriButton;
+    private JButton utenteButton;
+    private JPanel profilePanel;
+}
