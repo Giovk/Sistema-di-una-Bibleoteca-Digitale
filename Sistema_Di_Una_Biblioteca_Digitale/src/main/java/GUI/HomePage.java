8b470357469e220d94444874697eabfd8f38d0ee
genny2002 <002genny@gmail.com>
2023-06-02 20:07:05 +0200

    Update HomePage.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/HomePage.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/HomePage.java
@@ -21,27 +21,26 @@ public class HomePage {
 
     public HomePage(JFrame frameC, Controller controller) {
         text1.setText("Ciao " + controller.getUsername() + ", Sei nella Homepage!");
-        utenteMenu = new JPopupMenu();
-        JMenuItem utenteExit = new JMenuItem("Logout");
-        JMenuItem utenteProfilo = new JMenuItem("Profilo");
-        JMenuItem utenteLibrerie = new JMenuItem("Librerie");
-        utenteMenu.add(utenteProfilo);
-        utenteMenu.add(utenteLibrerie);
-        utenteMenu.add(utenteExit);
+        utenteMenu = new JPopupMenu();  //crea il menu 'utenteMenu'
+        JMenuItem utenteExit = new JMenuItem("Logout"); //crea la voce del menu "Logout"
+        JMenuItem utenteProfilo = new JMenuItem("Profilo"); //crea la voce del menu "Profilo"
+        JMenuItem utenteLibrerie = new JMenuItem("Librerie");   //crea la voce del menu "Librerie"
+        utenteMenu.add(utenteProfilo);  //aggiunge la voce 'utenteProfilo' al menu 'utenteMenu'
+        utenteMenu.add(utenteLibrerie); //aggiunge la voce 'utenteLibrerie' al menu 'utenteMenu'
+        utenteMenu.add(utenteExit); //aggiunge la voce 'utenteProfilo' al menu 'utenteExit'
 
-        if (controller.getPartitaIva() == null) {
-            utenteLibrerie.setVisible(false);
-            System.out.println(controller.getPartitaIva());
+        if (controller.getPartitaIva() == null) {   //controlla se la partita IVA dell'utente è nulla
+            utenteLibrerie.setVisible(false);   //rende invisibile la voce di menu 'utenteLibrerie'
         }
 
         frame = new JFrame("Homepage");
-        frame.setUndecorated(true);
+        frame.setUndecorated(true); //abilita le decorazioni del frame
         frame.setContentPane(homepagePanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setSize(1280, 720);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
+        frame.setSize(1280, 720);   //imposta largezza e altezza del frame
+        frame.setLocationRelativeTo(null); //posiziona il frame al centro dello schermo
+        frame.setResizable(false); //evita che l'utente modifichi le dimensioni del frame
         frame.setVisible(true);
 
         homeButton.addActionListener(new ActionListener() {
@@ -60,7 +59,7 @@ public class HomePage {
             @Override
             public void mousePressed(MouseEvent e) {
                 if (e.getButton() == MouseEvent.BUTTON1) {
-                    utenteMenu.show(utenteButton, utenteButton.getX(), utenteButton.getY() + 25);
+                    utenteMenu.show(utenteButton, utenteButton.getX(), utenteButton.getY() + 25); //mostra le voci del menu 'utenteMenu'
                 }
             }
         });
@@ -68,22 +67,21 @@ public class HomePage {
         utenteProfilo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Profilo pf = new Profilo(frameC, controller);
-                pf.frame.setVisible(true);
-                frame.setVisible(false);
+                Profilo pf = new Profilo(frameC, controller); //chiama il frame 'pf'
+                pf.frame.setVisible(true);  //rende visible il frame 'pf'
+                frame.setVisible(false);    //rende invisibile il frame
                 frame.dispose();
 
             }
         });
+
         utenteExit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                frameC.setVisible(true);
-                frame.setVisible(false);
+                frameC.setVisible(true); //rende visibile il frame chiamante
+                frame.setVisible(false);    //rende invisibile il frame
                 frame.dispose();
             }
         });
-
     }
-
 }
