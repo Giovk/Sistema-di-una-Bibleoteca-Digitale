e0d06e9ac63c08bf1ec0df47432d2aa2ed021c87
genny2002 <002genny@gmail.com>
2023-06-02 15:06:40 +0200

    Update Utente.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Utente.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/Model/Utente.java
@@ -51,4 +51,11 @@ public class Utente {
     public void  setDataNascita(String nDataNascita){
         dataNascita = nDataNascita;
     }
+
+    public  String getUsername(){return username;}
+    public  String getPartitaIVA(){return partitaIVA;}
+    public  String getPassword(){return password;}
+    public  String getNome(){return nome;}
+    public  String getCognome(){return cognome;}
+    public  String getEmail(){return email;}
 }
