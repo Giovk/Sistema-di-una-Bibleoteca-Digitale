807cb410b34dd931abf12698f1bcbd33f5977aec
genny2002 <002genny@gmail.com>
2023-06-02 20:07:01 +0200

    Update UtenteDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/UtenteDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/UtenteDAO.java
@@ -5,8 +5,8 @@ import Model.Utente;
 import java.sql.ResultSet;
 
 public interface UtenteDAO {
-    public void addUtenteDB(String email, String nome, String cognome, String Username, String password, String dataNascita, String partitaIVA);
-    public int validaUtenteDB(String userEmail, String password);
+    public void addUtenteDB(String email, String nome, String cognome, String Username, String password, String dataNascita, String partitaIVA);//inserisce un nuovo utente nel DB
+    public int validaUtenteDB(String userEmail, String password);//ritorna il numero di utenti registrati con username o email 'userMail' e con password 'password'
     public ResultSet getUtenteDB(String userEmail, String password);
     public int validaModEmailDB(String emailM);
     public  int validaModUsernameDB(String usernameM);
