95764d930e3eacdc8b1fe4a6a43619c0a2c78ffd
genny2002 <002genny@gmail.com>
2023-06-02 20:07:11 +0200

    Update LSForm.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/LSForm.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/LSForm.java
@@ -18,31 +18,33 @@ public class LSForm {
 
     public LSForm() {
         controller = new Controller();
+
         btLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                LSDialog lsDialog = new LSDialog(0, frame, controller);
-                frame.setEnabled(false);
+                LSDialog lsDialog = new LSDialog(0, frame, controller); //chiama il frame 'lsDialog'
+                frame.setEnabled(false); //disabilita il frame
             }
         });
+
         btSignUp.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                LSDialog lsDialog = new LSDialog(1, frame, controller);
-                frame.setEnabled(false);
+                LSDialog lsDialog = new LSDialog(1, frame, controller); //chiama il frame 'lsDialog'
+                frame.setEnabled(false);//disabilita il frame
             }
         });
     }
 
     public static void main(String[] args) {
         frame = new JFrame("Biblioteca Digitale");
-        frame.setUndecorated(true);
+        frame.setUndecorated(true); //abilita le decorazioni del frame
         frame.setContentPane(new LSForm().lsPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setSize(1280, 720);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
+        frame.setSize(1280, 720);   //imposta largezza e altezza del frame
+        frame.setLocationRelativeTo(null);  //posiziona il frame al centro dello schermo
+        frame.setResizable(false);  //evita che l'utente modifichi le dimensioni del frame
         frame.setVisible(true);
     }
 }
