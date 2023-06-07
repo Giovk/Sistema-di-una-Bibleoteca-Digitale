73c9d5da81d61c5b658af922fe053860987f47f1
genny2002 <002genny@gmail.com>
2023-06-08 00:24:17 +0200

    Update BooksPage.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/BooksPage.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/BooksPage.java
@@ -30,55 +30,59 @@ public class BooksPage {
     private JPanel jpanel;
 
     public BooksPage(JFrame frameC, Controller controller){
-        ArrayList<String> isbnList = controller.getLibriISBN();
-        ArrayList<String> titoloList = controller.getLibriTitolo();
-        ArrayList<String> genereList = controller.getLibriGenere();
-        ArrayList<String> linguaList = controller.getLibriLingua();
-        ArrayList<String> editoreList = controller.getLibriEditore();
-        ArrayList<String> dataPubblicazioneList = controller.getLibriDataPubblicazione();
-        ArrayList<String> autoreNomeList = controller.getAutoriLibroNome();
-        ArrayList<String> autoreCognomeList = controller.getAutoriLibroCognome();
-        ArrayList<String> collanaList = controller.getCollanaNome();
+        ArrayList<String> isbnList = controller.getLibriISBN(); //ISBN di tutti i libri nel DB
+        ArrayList<String> titoloList = controller.getLibriTitolo(); //titoli di tutti i libri nel DB
+        ArrayList<String> genereList = controller.getLibriGenere(); //generi di tutti i libri nel DB
+        ArrayList<String> linguaList = controller.getLibriLingua(); //lingue di tutti i libri nel DB
+        ArrayList<String> editoreList = controller.getLibriEditore();   //editori di tutti i libri nel DB
+        ArrayList<String> dataPubblicazioneList = controller.getLibriDataPubblicazione();   //date di pubblicazione di tutti i libri nel DB
+        ArrayList<String> autoreNomeList = controller.getAutoriLibroNome(); //nomi di tutti gli autori dei libti nel DB
+        ArrayList<String> autoreCognomeList = controller.getAutoriLibroCognome();   //cognomi di tutti gli autori dei libti nel DB
+        ArrayList<String> collanaList = controller.getCollanaNome();    //collane di libri nel DB
 
 
         frame = new JFrame("Biblioteca Digitale");
-        frame.setUndecorated(true);
+        frame.setUndecorated(true); //abilita le decorazioni del frame
         frame.setContentPane(jpanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
 
-        ArrayList<String> distinctGenereList = new ArrayList<String>();
-        for (int i = 0; i < genereList.size(); i++){
-            if(!distinctGenereList.contains(genereList.get(i))) distinctGenereList.add(genereList.get(i));
+        ArrayList<String> distinctGenereList = new ArrayList<String>(); //contiene tutti i generi dei libri senza duplicati
+
+        for (int i = 0; i < genereList.size(); i++){    //scorre l'ArrayList di tutti i generi dei libri
+            if(!distinctGenereList.contains(genereList.get(i))) distinctGenereList.add(genereList.get(i));  //se 'distinctGenereList' non contiene l'i-esimo elemento di genereList, allora lo inserisce in 'distinctGenereList'
         }
 
-        genereCB.setModel(new DefaultComboBoxModel<String>(distinctGenereList.toArray(new String[distinctGenereList.size()])));
-        genereCB.setSelectedIndex(-1);
+        genereCB.setModel(new DefaultComboBoxModel<String>(distinctGenereList.toArray(new String[distinctGenereList.size()]))); //inserisce tutti gli elementi di 'distinctGenereList' come voci del JComboBox 'genereCB'
+        genereCB.setSelectedIndex(-1);  //permette di avere 'genereCB' non selezionato
 
-        ArrayList<String> linkAutoreList = new ArrayList<String>();
-        ArrayList<String> distinctAutoreList = new ArrayList<String>();
-        for (int i = 0; i < autoreNomeList.size(); i++){
-            linkAutoreList.add(autoreNomeList.get(i) + " " + autoreCognomeList.get(i));
-            if(!distinctAutoreList.contains(linkAutoreList.get(i))) distinctAutoreList.add(linkAutoreList.get(i));
+        ArrayList<String> linkAutoreList = new ArrayList<String>(); //contiene i nomi e cognomi conatenati di tutti gli autori dei libri
+        ArrayList<String> distinctAutoreList = new ArrayList<String>(); //contiene i nomi e cognomi conatenati di tutti gli autori dei libri senza duplicati
+
+        for (int i = 0; i < autoreNomeList.size(); i++){    //scorre 'autoreNomeList' e 'autoreNomeList'
+            linkAutoreList.add(autoreNomeList.get(i) + " " + autoreCognomeList.get(i)); //aggiunge in 'linkAutoreList' il nome e cognome concatenati del i-esimo autore
+
+            if(!distinctAutoreList.contains(linkAutoreList.get(i))) distinctAutoreList.add(linkAutoreList.get(i));  //se l'i-esimo elemento di 'linkAutoreList' non è contenuto in 'distinctAutoreList', allora lo inserisce in 'distinctAutoreList'
         }
 
-        autoreCB.setModel(new DefaultComboBoxModel<String>(distinctAutoreList.toArray(new String[distinctAutoreList.size()])));
-        autoreCB.setSelectedIndex(-1);
+        autoreCB.setModel(new DefaultComboBoxModel<String>(distinctAutoreList.toArray(new String[distinctAutoreList.size()]))); //inserisce tutti gli elementi di 'distinctAutoreList' come voci del JComboBox 'autoreCB'
+        autoreCB.setSelectedIndex(-1);  //permette di avere 'autoreCB' non selezionato
 
-        collanaCB.setModel(new DefaultComboBoxModel<String>(collanaList.toArray(new String[collanaList.size()])));
-        collanaCB.setSelectedIndex(-1);
+        collanaCB.setModel(new DefaultComboBoxModel<String>(collanaList.toArray(new String[collanaList.size()])));  //inserisce tutti gli elementi di 'collanaList' come voci del JComboBox 'collanaCB'
+        collanaCB.setSelectedIndex(-1); //permette di avere 'collanaCB' non selezionato
 
         booksTable.setModel(new DefaultTableModel(new Object[][]{}, new String[]{"ISBN", "Titolo", "Genere", "Lingua", "Editore", "Data di pubblicazione"}));
         DefaultTableModel model = (DefaultTableModel) booksTable.getModel();
+
         if (isbnList != null && titoloList != null && genereList != null && linguaList != null && editoreList != null && dataPubblicazioneList != null){
             for (int i = 0; i < isbnList.size(); i++){
                 model.addRow(new Object[]{isbnList.get(i), titoloList.get(i), genereList.get(i), linguaList.get(i), editoreList.get(i), dataPubblicazioneList.get(i)});
             }
         }
 
-        frame.setSize(1280, 720);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
+        frame.setSize(1280, 720);   //imposta larghezza e altezza del frame
+        frame.setLocationRelativeTo(null);  //posiziona il frame al centro dello schermo
+        frame.setResizable(false);  //evita che l'utente modifichi le dimensioni del frame
         frame.setVisible(true);
 
     }
