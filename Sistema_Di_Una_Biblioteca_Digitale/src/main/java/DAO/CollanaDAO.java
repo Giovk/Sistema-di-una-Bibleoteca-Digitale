ae7619d300056f6f6827728f4c3a0b629ad0cf88
genny2002 <002genny@gmail.com>
2023-06-08 19:55:07 +0200

    Update CollanaDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/CollanaDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/CollanaDAO.java
@@ -3,6 +3,6 @@ package DAO;
 import java.sql.ResultSet;
 
 public interface CollanaDAO {
-    public ResultSet getCollanaDB();
-    public void chiudiConnessione();
+    public ResultSet getCollanaDB();    //ritorna i dati di tutte le collane nel DB
+    public void chiudiConnessione();    //chiude la connessione al DB
 }
