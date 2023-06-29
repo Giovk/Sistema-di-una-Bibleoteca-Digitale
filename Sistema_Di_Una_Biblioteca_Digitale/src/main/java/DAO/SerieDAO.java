311ddf510274a0efc63d8514c3c71b27d54757a1
genny2002 <002genny@gmail.com>
2023-06-29 11:16:33 +0200

    Update SerieDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/SerieDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/SerieDAO.java
@@ -3,6 +3,6 @@ package DAO;
 import java.sql.ResultSet;
 
 public interface SerieDAO {
-    public ResultSet getSerieDB();
-    public void chiudiConnessione();
+    public ResultSet getSerieDB();  //ritorna i dati di tutte le serie nel DB
+    public void chiudiConnessione();    //chiude la connessione al DB
 }
