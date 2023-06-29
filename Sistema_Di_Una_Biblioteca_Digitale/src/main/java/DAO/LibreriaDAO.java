336c392d5ed51669dee92b3d3d178b93d5f334e5
genny2002 <002genny@gmail.com>
2023-06-29 11:16:24 +0200

    Update LibreriaDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/LibreriaDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/LibreriaDAO.java
@@ -3,6 +3,6 @@ package DAO;
 import java.sql.ResultSet;
 
 public interface LibreriaDAO {
-    public ResultSet disponibilitaDB(String isbn);
+    public ResultSet disponibilitaDB(String isbn);  //ritorna le disponibilità del libro con ISBN 'isbn' nelle librerie
     public void chiudiConnessione();
 }
