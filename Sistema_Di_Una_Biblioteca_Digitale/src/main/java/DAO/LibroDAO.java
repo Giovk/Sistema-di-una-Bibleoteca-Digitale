6c6a08f1c7b54e4dc35fc47c6b62eac51b684691
genny2002 <002genny@gmail.com>
2023-06-29 11:16:26 +0200

    Update LibroDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/LibroDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/LibroDAO.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 
 public interface LibroDAO {
     public ResultSet getLibriDB();  //ritorna i dati di tutti i libri nel DB
-    public ResultSet getLibriDB(String collana);  //ritorna i dati di tutti i libri nel DB
+    public ResultSet getLibriDB(String collana);  //ritorna i dati di tutti i libri della collana 'collana' nel DB
     //public ResultSet getLibroAutoriDB(String s);    //ritorna i dati del libro con ISBN 's'
-    public ResultSet getLibriSerieDB(String s);
+    public ResultSet getLibriSerieDB(String s); //ritorna i dati di tutti i libri della serie con ISBN 's' nel DB
     public void chiudiConnessione(); //chiude la connessione al DB
 }
