d899784370262cda392c17cb0e254a55126d16ee
genny2002 <002genny@gmail.com>
2023-06-29 11:16:29 +0200

    Update PresentazioneDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/PresentazioneDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/PresentazioneDAO.java
@@ -3,6 +3,6 @@ package DAO;
 import java.sql.ResultSet;
 
 public interface PresentazioneDAO {
-    public ResultSet getPresentazioneDB(String isbn);
-    public void chiudiConnessione();
+    public ResultSet getPresentazioneDB(String isbn);   //ritorna i dati di tutte le presentazioni del libro on ISBN 'isbn' nel DB
+    public void chiudiConnessione();    //ritorna i dati di tutti i libri nel DB
 }
