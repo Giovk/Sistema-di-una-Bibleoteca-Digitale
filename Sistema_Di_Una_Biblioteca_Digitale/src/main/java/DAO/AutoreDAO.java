f6fedb60eff1d0eb6d445fa60c694934ad7f7719
genny2002 <002genny@gmail.com>
2023-07-04 09:25:01 +0200

    Update AutoreDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/AutoreDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/AutoreDAO.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public interface AutoreDAO {
-    //public ResultSet getAutoriLibroDB();    //ritorna i dati di tutti gli autori di libri nel DB
     public ResultSet getAutoriDB(String isbn);  //ritorna i dati di tutti gli autori nel DB dell libro con ISBN 'isbn'
     public void chiudiConnessione();    //chiude la connessione al DB
 }
