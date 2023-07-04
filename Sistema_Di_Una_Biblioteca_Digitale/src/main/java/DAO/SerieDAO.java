5514d54fed5dd4e33b856d2f980941c9a40fd5c4
genny2002 <002genny@gmail.com>
2023-07-04 09:57:25 +0200

    Update SerieDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/SerieDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/SerieDAO.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public interface SerieDAO {
     public ResultSet getSerieDB();  //ritorna i dati di tutte le serie nel DB
-    public ArrayList<String> getSerieGenereDB();
-    public ArrayList<String> getSerieAutoriDB();
+    public ArrayList<String> getSerieGenereDB();    //ritorna tutti i generi dei libri che sono inseriti in una serie
+    public ArrayList<String> getSerieAutoriDB();    //ritorna tutti gli autori dei libri che sono inseriti in una serie
     public void chiudiConnessione();    //chiude la connessione al DB
 }
