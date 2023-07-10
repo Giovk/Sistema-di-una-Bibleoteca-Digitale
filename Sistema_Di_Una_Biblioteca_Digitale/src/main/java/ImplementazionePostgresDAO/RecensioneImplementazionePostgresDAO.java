8ccb9de16107684eeb6745934fe0e9d105cd5524
genny2002 <002genny@gmail.com>
2023-07-10 22:02:49 +0200

    Update RecensioneImplementazionePostgresDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/ImplementazionePostgresDAO/RecensioneImplementazionePostgresDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/ImplementazionePostgresDAO/RecensioneImplementazionePostgresDAO.java
@@ -21,17 +21,17 @@ public class RecensioneImplementazionePostgresDAO implements RecensioneDAO {
     }
 
     @Override
-    public float valutazioneMediaLibroDB(String isbn) {
-        ResultSet rs = null;    //autori trovati
-        float vm = 0;
+    public float valutazioneMediaLibroDB(String isbn) { //ritorna la media delle valutazioni del libro con isbn 'isbn'
+        ResultSet rs = null;    //media trovata
+        float vm = 0;   //valore medio delle valutazioni
 
         try {
             PreparedStatement valutazioneMediaLibroPS = connection.prepareStatement(
-                    "SELECT AVG(valutazione) FROM recensione_l WHERE isbn = '"+isbn+"'"
+                    "SELECT AVG(valutazione) FROM recensione_l WHERE isbn = '"+isbn+"'" //prepara la query che calcola il valore medio delle valutazioni del libro
             );
             rs = valutazioneMediaLibroPS.executeQuery(); //esegue la query
 
-            while(rs.next()){    //scorre il ResultSet 'rs' contenente i generi
+            while(rs.next()){    //scorre il ResultSet 'rs' contenente la media
                 vm = rs.getFloat(1);
             }
 
