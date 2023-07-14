4f55c52ab95ae8c6de3e350713a91d6b38dfd277
genny2002 <002genny@gmail.com>
2023-07-14 08:56:32 +0200

    Update RecensioneDAO.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/RecensioneDAO.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/DAO/RecensioneDAO.java
@@ -4,9 +4,9 @@ import java.sql.ResultSet;
 
 public interface RecensioneDAO {
     public float valutazioneMediaLibroDB(String isbn);  //ritorna la media delle valutazioni del libro con isbn 'isbn'
-    public boolean likeLibroDB(String isbn, String user);
-    public boolean changeLikeDB(boolean like, String isbn, String user);
-    public void addRecensioneLibroDB(int valutazione, String text, String isbn, String user);
-    public ResultSet allRecWithCommentDB(String isbn);
+    public boolean likeLibroDB(String isbn, String user);   //controlla se l'utente 'user' ha il libro 'isbn' tra i preferiti
+    public boolean changeLikeDB(boolean like, String isbn, String user);    //toglie/mette nei preferiti dell'utente 'user' il libro 'isbn' e ritone l'opposto di 'like'
+    public void addRecensioneLibroDB(int valutazione, String text, String isbn, String user);   //aggiunge/aggiorna una recensione con 'valutazione' e 'testo' fatta dall'utente 'user' al libro 'isbn'
+    public ResultSet allRecWithCommentDB(String isbn);  //ritorna tutte le recensioni con un testo fatte al libro 'isbn'
     public void chiudiConnessione();    //chiude la connessione al DB
 }
