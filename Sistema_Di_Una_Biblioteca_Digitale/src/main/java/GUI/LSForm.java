647f3e6a58ee4000c19669bdf6d122cd5b1c0897
genny2002 <002genny@gmail.com>
2023-11-14 00:20:38 +0100

    Update LSForm.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/LSForm.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/LSForm.java
@@ -2,13 +2,10 @@ package GUI;
 
 import Controller.Controller;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.net.URL;
-import java.nio.Buffer;
+
 
 public class LSForm {
     private Controller controller;
@@ -18,7 +15,6 @@ public class LSForm {
     private JLabel txtPF;
     private JLabel image;
     private JPanel lsPanel;
-    private ImageIcon image2;
 
     public LSForm() {
         controller = new Controller();
@@ -31,12 +27,12 @@ public class LSForm {
         frame.setSize(1280, 720);   //imposta larghezza e altezza del frame
         frame.setLocationRelativeTo(null);  //posiziona il frame al centro dello schermo
         frame.setResizable(false);  //evita che l'utente modifichi le dimensioni del frame
-        frame.setVisible(true);
+        frame.setVisible(true); //rende visibile il frame
+
         btSignIn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //LSDialog lsDialog = new LSDialog(0, frame, controller); //chiama il frame 'lsDialog'
-                NewLoginForm newLoginForm = new NewLoginForm(0, frame, controller);
+                NewLoginForm newLoginForm = new NewLoginForm(0, frame, controller); //chiama il frame 'NewLoginForm'
                 frame.setEnabled(false); //disabilita il frame
             }
         });
@@ -44,9 +40,8 @@ public class LSForm {
         btSignUp.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //LSDialog lsDialog = new LSDialog(1, frame, controller); //chiama il frame 'lsDialog'
-                NewLoginForm newLoginForm = new NewLoginForm(1, frame, controller);
-                frame.setEnabled(false);//disabilita il frame
+                NewLoginForm newLoginForm = new NewLoginForm(1, frame, controller); //chiama il frame 'NewLoginForm'
+                frame.setEnabled(false);    //disabilita il frame
             }
         });
 
@@ -54,11 +49,11 @@ public class LSForm {
             @Override
             public void mouseExited(MouseEvent e) {
                 super.mouseExited(e);
-                ImageIcon bt1igm = new ImageIcon(this.getClass().getResource("/button-type1.png"));
-                Image resbt1img = bt1igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH);
+                ImageIcon bt1igm = new ImageIcon(this.getClass().getResource("/button-type1.png")); //carica l'immagine nel percorso /button-type1.png
+                Image resbt1img = bt1igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH); //imposta le dimensioni dell'immagine
                 bt1igm = new ImageIcon(resbt1img);
-                btSignIn.setIcon(bt1igm);
-                btSignIn.setForeground(Color.decode("#EEEEEE"));
+                btSignIn.setIcon(bt1igm);   //imposta l'icona del JButton 'btSignIn' con l'immagine
+                btSignIn.setForeground(Color.decode("#EEEEEE"));    //imposta il colore dello sfondo del JButton 'btSignIn'
 
             }
         });
@@ -67,11 +62,11 @@ public class LSForm {
             @Override
             public void mouseEntered(MouseEvent e) {
                 super.mouseEntered(e);
-                ImageIcon bt1igm = new ImageIcon(this.getClass().getResource("/button-type2.png"));
-                Image resbt1img = bt1igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH);
+                ImageIcon bt1igm = new ImageIcon(this.getClass().getResource("/button-type2.png")); //carica l'immagine nel percorso /button-type2.png
+                Image resbt1img = bt1igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH); //imposta le dimensioni dell'immagine
                 bt1igm = new ImageIcon(resbt1img);
-                btSignIn.setIcon(bt1igm);
-                btSignIn.setForeground(Color.decode("#D6D4D4"));
+                btSignIn.setIcon(bt1igm);   //imposta l'icona del JButton 'btSignIn' con l'immagine
+                btSignIn.setForeground(Color.decode("#D6D4D4"));    //imposta il colore dello sfondo del JButton 'btSignIn'
 
             }
         });
@@ -80,11 +75,11 @@ public class LSForm {
             @Override
             public void mouseExited(MouseEvent e) {
                 super.mouseExited(e);
-                ImageIcon bt2igm = new ImageIcon(this.getClass().getResource("/button-type1.png"));
-                Image resbt2img = bt2igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH);
+                ImageIcon bt2igm = new ImageIcon(this.getClass().getResource("/button-type1.png")); //carica l'immagine nel percorso /button-type1.png
+                Image resbt2img = bt2igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH); //imposta le dimensioni dell'immagine
                 bt2igm = new ImageIcon(resbt2img);
-                btSignUp.setIcon(bt2igm);
-                btSignUp.setForeground(Color.decode("#EEEEEE"));
+                btSignUp.setIcon(bt2igm);   //imposta l'icona del JButton 'btSignUp' con l'immagine
+                btSignUp.setForeground(Color.decode("#EEEEEE"));    //imposta il colore dello sfondo del JButton 'btSignUp'
 
             }
         });
@@ -93,11 +88,11 @@ public class LSForm {
             @Override
             public void mouseEntered(MouseEvent e) {
                 super.mouseEntered(e);
-                ImageIcon bt2igm = new ImageIcon(this.getClass().getResource("/button-type2.png"));
-                Image resbt2img = bt2igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH);
+                ImageIcon bt2igm = new ImageIcon(this.getClass().getResource("/button-type2.png")); //carica l'immagine nel percorso /button-type2.png
+                Image resbt2img = bt2igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH); //imposta le dimensioni dell'immagine
                 bt2igm = new ImageIcon(resbt2img);
-                btSignUp.setIcon(bt2igm);
-                btSignUp.setForeground(Color.decode("#D6D4D4"));
+                btSignUp.setIcon(bt2igm);   //imposta l'icona del JButton 'btSignUp' con l'immagine
+                btSignUp.setForeground(Color.decode("#D6D4D4"));    //imposta il colore delllo sfondo del JButton 'btSignUp'
             }
         });
 
@@ -105,7 +100,7 @@ public class LSForm {
             @Override
             public void mouseEntered(MouseEvent e) {
                 super.mouseEntered(e);
-                txtPF.setForeground(Color.decode("#D6D4D4"));
+                txtPF.setForeground(Color.decode("#D6D4D4"));   //imposta il colore dello sfondo della JLabel 'txtPF'
             }
         });
 
@@ -113,61 +108,46 @@ public class LSForm {
             @Override
             public void mouseExited(MouseEvent e) {
                 super.mouseExited(e);
-                txtPF.setForeground(Color.decode("#EEEEEE"));
+                txtPF.setForeground(Color.decode("#EEEEEE"));   //imposta il colore dello sfondo della JLabel 'txtPF'
             }
         });
     }
-
-    /*public static void main(String[] args) {
-        frame = new JFrame("Biblioteca Digitale");
-        frame.setUndecorated(true); //abilita le decorazioni del frame
-        frame.setContentPane(new LSForm().lsPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setSize(1280, 720);   //imposta larghezza e altezza del frame
-        frame.setLocationRelativeTo(null);  //posiziona il frame al centro dello schermo
-        frame.setResizable(false);  //evita che l'utente modifichi le dimensioni del frame
-        frame.setVisible(true);
-    }*/
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
         txtPF = new JLabel();
-        txtPF.setForeground(Color.decode("#EEEEEE"));
+        txtPF.setForeground(Color.decode("#EEEEEE"));   //imposta il colore dello sfondo della JLabel 'txtPF'
 
         image = new JLabel();
-        image.setText("");
+        image.setText("");  //imposta il testo della 'JLabel'
 
-
-        ImageIcon lb1igm = new ImageIcon(this.getClass().getResource("/b.png"));
-        Image reslb1img = lb1igm.getImage().getScaledInstance(300, 169, Image.SCALE_SMOOTH);
+        ImageIcon lb1igm = new ImageIcon(this.getClass().getResource("/b.png"));    //carica l'immagine nel percorso /b.png
+        Image reslb1img = lb1igm.getImage().getScaledInstance(300, 169, Image.SCALE_SMOOTH);  //imposta le dimensioni dell'immagine
         lb1igm = new ImageIcon(reslb1img);
-        image.setIcon(lb1igm);
-
+        image.setIcon(lb1igm);  //imposta l'icona della JLabel 'image' con l'immagine
 
         btSignIn = new JButton();
-        btSignIn.setContentAreaFilled(false);
-        btSignIn.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-        btSignIn.setMargin(new Insets(0, 0, 0, 0));
-        ImageIcon bt1igm = new ImageIcon(this.getClass().getResource("/button-type1.png"));
-        Image resbt1img = bt1igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH);
+        btSignIn.setContentAreaFilled(false);   //toglie lo sfondo dell JButton 'btSignIn'
+        btSignIn.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));    //toglie il bordo del JButton 'btSignIn'
+        btSignIn.setMargin(new Insets(0, 0, 0, 0)); //toglie il margine all'interno del JButton 'btSignIn'
+        ImageIcon bt1igm = new ImageIcon(this.getClass().getResource("/button-type1.png")); //carica l'immagine nel percorso /button-type1.png
+        Image resbt1img = bt1igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH); //imposta le dimensioni dell'immagine
         bt1igm = new ImageIcon(resbt1img);
-        btSignIn.setIcon(bt1igm);
-        btSignIn.setHorizontalTextPosition(JButton.CENTER);
-        btSignIn.setVerticalTextPosition(JButton.CENTER);
-        btSignIn.setForeground(Color.decode("#EEEEEE"));
+        btSignIn.setIcon(bt1igm);   //imposta l'icona del JButton 'btSignIn' con l'immagine
+        btSignIn.setHorizontalTextPosition(JButton.CENTER); //centra orizzontalmente il contenuto dell JButton 'btSignIn'
+        btSignIn.setVerticalTextPosition(JButton.CENTER);   //centra verticalmente il contenuto dell JButton 'btSignIn'
+        btSignIn.setForeground(Color.decode("#EEEEEE"));    //imposta il colore dello sfondo del JButton 'btSignIn'
 
         btSignUp = new JButton();
-        btSignUp.setContentAreaFilled(false);
-        btSignUp.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-        btSignUp.setMargin(new Insets(0, 0, 0, 0));
-        ImageIcon bt2igm = new ImageIcon(this.getClass().getResource("/button-type1.png"));
-        Image resbt2img = bt2igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH);
+        btSignUp.setContentAreaFilled(false);   //toglie lo sfondo dell JButton 'btSignUp'
+        btSignUp.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));    //toglie il bordo dell JButton 'btSignUp'
+        btSignUp.setMargin(new Insets(0, 0, 0, 0)); //toglie il margine all'interno del JButton 'btSignUp'
+        ImageIcon bt2igm = new ImageIcon(this.getClass().getResource("/button-type1.png")); //carica l'immagine nel percorso /button-type1.png
+        Image resbt2img = bt2igm.getImage().getScaledInstance(150, 41, Image.SCALE_SMOOTH); //imposta le dimensioni dell'immagin
         bt2igm = new ImageIcon(resbt2img);
-        btSignUp.setIcon(bt2igm);
-        btSignUp.setHorizontalTextPosition(JButton.CENTER);
-        btSignUp.setVerticalTextPosition(JButton.CENTER);
-        btSignUp.setForeground(Color.decode("#EEEEEE"));
+        btSignUp.setIcon(bt2igm);   //imposta l'icona del JButton 'btSignIn' con l'immagine
+        btSignUp.setHorizontalTextPosition(JButton.CENTER); //centra orizzontalmente il contenuto dell JButton 'btSignUp'
+        btSignUp.setVerticalTextPosition(JButton.CENTER);   //centra verticalmente il contenuto dell JButton 'btSignUp'
+        btSignUp.setForeground(Color.decode("#EEEEEE"));    //imposta il colore dello sfondo del JButton 'btSignUp'
     }
 }
-
