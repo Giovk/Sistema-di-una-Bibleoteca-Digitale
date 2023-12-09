ac3fbdd57792e0f7aa1f222ef4ebe50a45d084da
genny2002 <002genny@gmail.com>
2023-12-10 00:40:52 +0100

    Update AggiungiElementoForm.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/AggiungiElementoForm.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/AggiungiElementoForm.java
@@ -1933,7 +1933,7 @@ public class AggiungiElementoForm {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     initComponentsAutoriArticoli(panel2, controller.baseFontSize, controller.textFieldFont);    //inizializza tutti i componenti necessari per inserire gli autori dell'articolo
-                    panel2.revalidate();    //aggiorna il contenuto del JPanel 'panel2l'
+                    panel2.revalidate();    //aggiorna il contenuto del JPanel 'panel2'
                     panel2.repaint();   //ridisegna il JPanel 'articoliPanel'
                 }
             });
@@ -2423,7 +2423,7 @@ public class AggiungiElementoForm {
 
         calendarIssueIMG = new JLabel(calendarIco); //inizializza la JLabel 'calendarSerieIMG' dell'icona del calendario
 
-        articoliPanel = new JPanel();   //inizializza il JPanel 'articoliPaneld'
+        articoliPanel = new JPanel();   //inizializza il JPanel 'articoliPanel'
         articoliPanel.setLayout(new GridBagLayout());   //imposta il layout del JPanel 'articoliPanel'
         ((GridBagLayout)articoliPanel.getLayout()).columnWidths = new int[] {0, 0}; //le larghezze delle colonne del layout di 'articoliPanel' vengono impostate dinamicamente in base alle componenti contenute
         ((GridBagLayout)articoliPanel.getLayout()).rowHeights = new int[] {0, 0};   //le altezze delle righe del layout di 'articoliPanel' vengono impostate dinamicamente in base alle componenti contenute
