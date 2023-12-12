4679c434d743e521392a3efd1ade5fa4c69273a4
genny2002 <002genny@gmail.com>
2023-12-12 22:31:02 +0100

    Update NewShowMessageDialog.java

--- Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/NewShowMessageDialog.java
+++ Sistema_Di_Una_Biblioteca_Digitale/src/main/java/GUI/NewShowMessageDialog.java
@@ -1,7 +1,5 @@
 package GUI;
 
-import Controller.Controller;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -22,8 +20,10 @@ public class NewShowMessageDialog extends JDialog {
         this.setUndecorated(true);  //rimuove la decorazione della finestra
         this.setSize((int) (screenWidth/8.5333),(int) (screenHeight/7.2));  //imposta le dimensioni della finestra
         this.setLocationRelativeTo(null);   //posiziona il frame al centro dello schermo
-        Image icona = new ImageIcon(this.getClass().getResource("/icon.png")).getImage();
-        this.setIconImage(icona);
+
+        Image icona = new ImageIcon(this.getClass().getResource("/icon.png")).getImage();   //carica l'immagine nel percorso /icon.png
+
+        this.setIconImage(icona);   //imposta la l'icona dell'applicazione
         setContentPane(contentPane);
         setModal(true); //imposta la finestra come modal
         getRootPane().setDefaultButton(okButton); //imposta il JButton 'okButton' come bottone predefinito della finestra
@@ -34,26 +34,28 @@ public class NewShowMessageDialog extends JDialog {
             case 1:
                 ImageIcon doneIco = new ImageIcon(this.getClass().getResource("/done.png"));    //carica l'immagine nel percorso /done.png
                 Image doneImg = doneIco.getImage().getScaledInstance((int) (screenWidth/51.2),(int) (screenHeight/28.8), Image.SCALE_SMOOTH);   //imposta le dimensioni dell'immagine
-                doneIco = new ImageIcon(doneImg);
+
+                doneIco = new ImageIcon(doneImg);   //reinizializza l'ImageIcon 'doneIco' con l'Image 'doneImg'
                 typeLabel.setIcon(doneIco); //imposta l'icona della JLabel 'typeLabel' con l'immagine
                 break;
 
             case 2:
                 ImageIcon errorIco = new ImageIcon(this.getClass().getResource("/error.png"));  //carica l'immagine nel percorso /error.png
                 Image errorImg = errorIco.getImage().getScaledInstance((int) (screenWidth/51.2),(int) (screenHeight/28.8), Image.SCALE_SMOOTH); //imposta le dimensioni dell'immagine
-                errorIco = new ImageIcon(errorImg);
+
+                errorIco = new ImageIcon(errorImg); //reinizializza l'ImageIcon 'errorIco' con l'Image 'errorImg'
                 typeLabel.setIcon(errorIco);    //imposta l'icona della JLabel 'typeLabel' con l'immagine
                 break;
 
             default:
-                typeLabel.setText("Image Not Found Or Not Selected");   //imposta il testo della JLabel typeLabel
+                typeLabel.setText("Image Not Found Or Not Selected");   //imposta il testo della JLabel 'typeLabel'
         }
 
-        typeMessageLabel.setFont(baseFontSize);
+        typeMessageLabel.setFont(baseFontSize); //imposta il font della JLabel 'typeMessageLabel'
         typeMessageLabel.setText(message);  //imposta il testo della JLabel 'typeMessageLabel' con 'message'
 
-        okButton.setMinimumSize(new Dimension((screenWidth/16), -1));
-        okButton.setFont(baseFontSize);
+        okButton.setMinimumSize(new Dimension((screenWidth/16), -1));   //imposta la dimensione minima del JButton 'okButton'
+        okButton.setFont(baseFontSize); //imposta il font del JButton 'okButton'
 
         okButton.addMouseListener(new MouseAdapter() {
             @Override
@@ -103,9 +105,9 @@ public class NewShowMessageDialog extends JDialog {
     private void onCancel() {   //chiude la finestra
         dispose();
     }//fine onCancel
-    public int getFontSize(){
-        // Calcola la dimensione del font in base alle dimensioni dello schermo
-        int fontSize = Math.min(screenWidth, screenHeight) / 50; // Modifica il coefficiente a seconda delle tue preferenze
+
+    public int getFontSize(){   //calcola le dimensioni del font in base alle dimensioni dello schermo
+        int fontSize = Math.min(screenWidth, screenHeight) / 50;    //dimensione del font
         return fontSize;
-    }
+    }//fine getFontSize
 }
